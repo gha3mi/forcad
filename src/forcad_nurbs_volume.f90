ba0393759b035556b012f84d20235c8b1849d4d1
Seyed Ali Ghasemi <info@gha3mi.com>
2024-04-10 23:51:56 +0200

    Add put_to_nurbs

--- src/forcad_nurbs_volume.f90
+++ src/forcad_nurbs_volume.f90
@@ -61,6 +61,7 @@ module forcad_nurbs_volume
         procedure :: insert_knots           !!> Insert knots into the knot vector
         procedure :: elevate_degree         !!> Elevate the degree of the NURBS volume
         procedure :: is_rational            !!> Check if the NURBS volume is rational
+        procedure :: put_to_nurbs           !!> Put a shape to a NURBS volume
 
         ! Shapes
         procedure :: set_hexahedron         !!> Set a hexahedron
@@ -1414,5 +1415,63 @@ contains
     end subroutine
     !===============================================================================
 
+
+    !===============================================================================
+    !> author: Seyed Ali Ghasemi
+    !> license: BSD 3-Clause
+    pure subroutine put_to_nurbs(this, X, elemConn)
+        class(nurbs_volume), intent(inout) :: this
+        real(rk), intent(in) :: X(:,:)
+        integer, intent(in) :: elemConn(:,:)
+        integer :: i, j
+        real(rk), dimension(:), allocatable :: Tgc1, Tgc2, Tgc3, Tgc
+        real(rk), dimension(:,:), allocatable :: Xt
+        real(rk) :: min_X1, max_X1, min_X2, max_X2, min_X3, max_X3
+
+        ! Assuming knot vectors are in the range [0,1]
+        ! Normalize the X coordinates to the range [0,1]
+        allocate(Xt(size(X,1), size(X,2)))
+        min_X1 = minval(X(:,1))
+        max_X1 = maxval(X(:,1))
+
+        min_X2 = minval(X(:,2))
+        max_X2 = maxval(X(:,2))
+
+        min_X3 = minval(X(:,3))
+        max_X3 = maxval(X(:,3))
+
+        Xt(:,1) = (X(:,1) -  min_X1) / (max_X1 -  min_X1)
+        Xt(:,2) = (X(:,2) -  min_X2) / (max_X2 -  min_X2)
+        Xt(:,3) = (X(:,3) -  min_X3) / (max_X3 -  min_X3)
+
+        allocate(this%Xg(size(Xt,1), size(this%Xc,2)))
+
+        if (allocated(this%Wc)) then ! NURBS volume
+            do i = 1, size(Xt, 1)
+                Tgc1 = basis_bspline(Xt(i,1), this%knot1, this%nc(1), this%degree(1))
+                Tgc2 = basis_bspline(Xt(i,2), this%knot2, this%nc(2), this%degree(2))
+                Tgc3 = basis_bspline(Xt(i,3), this%knot3, this%nc(3), this%degree(3))
+                Tgc = kron(Tgc3, kron(Tgc2, Tgc1))
+                Tgc = Tgc*(this%Wc/(dot_product(Tgc,this%Wc)))
+                do j = 1, size(this%Xc, 2)
+                    this%Xg(i,j) = dot_product(Tgc,this%Xc(:,j))
+                end do
+            end do
+        else ! B-Spline volume
+            do i = 1, size(Xt, 1)
+                Tgc1 = basis_bspline(Xt(i,1), this%knot1, this%nc(1), this%degree(1))
+                Tgc2 = basis_bspline(Xt(i,2), this%knot2, this%nc(2), this%degree(2))
+                Tgc3 = basis_bspline(Xt(i,3), this%knot3, this%nc(3), this%degree(3))
+                Tgc = kron(Tgc3, kron(Tgc2, Tgc1))
+                do j = 1, size(this%Xc, 2)
+                    this%Xg(i,j) = dot_product(Tgc,this%Xc(:,j))
+                end do
+            end do
+        end if
+
+        call this%set_elem_Xg_vis(elemConn)
+
+    end subroutine
+    !===============================================================================
 
 end module forcad_nurbs_volume
