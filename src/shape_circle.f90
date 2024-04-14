655ec86e1d93cab92d0ebcdcd5a03fd3c54020d0
gha3mi <36126354+gha3mi@users.noreply.github.com>
2024-04-14 08:02:48 +0000

    Deploying to gh-pages from @ gha3mi/forcad@36f7cc08cb8f05010865642c0ce4816a52b78f8c 🚀

---
+++ src/shape_circle.f90
@@ -0,0 +1,35 @@
+program shape_circle
+
+    use forcad, only: rk, nurbs_curve
+
+    implicit none
+    type(nurbs_curve) :: shape
+
+    !-----------------------------------------------------------------------------
+    ! Setting up NURBS circle
+    !-----------------------------------------------------------------------------
+
+    !> Set a circle with radius 2.0 and center at [0.0, 0.0, 0.0]
+    call shape%set_circle(center = [0.0_rk, 0.0_rk, 0.0_rk], radius = 2.0_rk)
+
+    !> Export control points to a VTK file
+    call shape%export_Xc('vtk/shape_circle_Xc.vtk')
+
+    !-----------------------------------------------------------------------------
+    ! Creating circle
+    !-----------------------------------------------------------------------------
+
+    !> Generate the NURBS circle with a resolution of 100
+    call shape%create(res = 100)
+
+    !> Export the generated cirlce to a VTK file
+    call shape%export_Xg('vtk/shape_circle_Xg.vtk')
+
+    !-----------------------------------------------------------------------------
+    ! Finalizing
+    !-----------------------------------------------------------------------------
+
+    !> Finalize the NURBS curve object
+    call shape%finalize()
+
+end program
